09d6473acb05b45424e9fc6f3191af5a3f9c9ce1
Giulio Chessa <giulio.chessa@eni.com>
2023-09-16 08:22:03 +0200

    clean, simple, domain specific error mgmt

--- main.go
+++ main.go
@@ -53,55 +53,52 @@ func main() {
 	blobContainer, err := getBlobContainer(ctx)
 
 	var respErr *azcore.ResponseError
-	if err != nil {
-		if errors.As(err, &respErr) {
-			// Handle Error
-			if respErr.StatusCode == http.StatusNotFound {
-				fmt.Printf("Blob container could not be found, return code: %v\n", respErr.ErrorCode)
-				fmt.Println("Creating it now...")
-				blobContainer_new, err_new := createBlobContainers(ctx)
-				if err_new != nil {
-					log.Fatal(err_new)
-					return
-				}
-				log.Println("Created blob container:", *blobContainer_new.ID)
-				blobContainer_again, err_again := getBlobContainer(ctx)
-				if err_again != nil {
-					log.Fatal(err_again)
-					return
-				}
-				log.Println("Double check, blob container ID:", *blobContainer_again.ID)
-
-
-				currentTime := time.Now()
-				today := strings.Split(currentTime.Format(time.RFC3339Nano), "+")[0]+"Z"
-				fmt.Printf("today %v format should be 2017-05-24T11:42:03.1567373Z\n", today)
-				tomorrow := strings.Split( currentTime.Add(24 * time.Hour).Format(time.RFC3339Nano) , "+")[0]+"Z"
-				fmt.Printf("tomorrow %v format should be 2017-05-24T11:42:03.1567373Z\n", tomorrow)
-
-				client := storageClientFactory.NewAccountsClient()
-				sasToken, err := client.ListAccountSAS(ctx, resourceGroupName, storageAccountName, armstorage.AccountSasParameters{
-					KeyToSign:              to.Ptr("key1"),
-					SharedAccessExpiryTime: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, tomorrow); return t }()),
-					Permissions:            to.Ptr(armstorage.PermissionsR),
-					Protocols:              to.Ptr(armstorage.HTTPProtocolHTTPSHTTP),
-					ResourceTypes:          to.Ptr(armstorage.SignedResourceTypesS),
-					Services:               to.Ptr(armstorage.ServicesB),
-					SharedAccessStartTime:  to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, today); return t }()),
-				}, nil)
-				if err != nil {
-					log.Fatal(err)
-					return
-				}
-				token := *sasToken.AccountSasToken
-				fmt.Printf("SAS Token %v\n", token)
+	
+	if errors.As(err, &respErr) {
+		// Handle Error
+		if respErr.StatusCode == http.StatusNotFound {
+			fmt.Printf("Blob container could not be found, return code: %v\n", respErr.ErrorCode)
+			fmt.Println("Creating it now...")
+			blobContainer_new, err_new := createBlobContainers(ctx)
+			if err_new != nil {
+				log.Fatal(err_new)
 				return
-			} else {
-				fmt.Printf("Container Get status code %d error code: %v", respErr.StatusCode, respErr.ErrorCode)
-				log.Fatal(respErr)
 			}
+			log.Println("Created blob container:", *blobContainer_new.ID)
+			blobContainer_again, err_again := getBlobContainer(ctx)
+			if err_again != nil {
+				log.Fatal(err_again)
+				return
+			}
+			log.Println("Double check, blob container ID:", *blobContainer_again.ID)
+
+
+			currentTime := time.Now()
+			today := strings.Split(currentTime.Format(time.RFC3339Nano), "+")[0]+"Z"
+			fmt.Printf("today %v format should be 2017-05-24T11:42:03.1567373Z\n", today)
+			tomorrow := strings.Split( currentTime.Add(24 * time.Hour).Format(time.RFC3339Nano) , "+")[0]+"Z"
+			fmt.Printf("tomorrow %v format should be 2017-05-24T11:42:03.1567373Z\n", tomorrow)
+
+			client := storageClientFactory.NewAccountsClient()
+			sasToken, err := client.ListAccountSAS(ctx, resourceGroupName, storageAccountName, armstorage.AccountSasParameters{
+				KeyToSign:              to.Ptr("key1"),
+				SharedAccessExpiryTime: to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, tomorrow); return t }()),
+				Permissions:            to.Ptr(armstorage.PermissionsR),
+				Protocols:              to.Ptr(armstorage.HTTPProtocolHTTPSHTTP),
+				ResourceTypes:          to.Ptr(armstorage.SignedResourceTypesS),
+				Services:               to.Ptr(armstorage.ServicesB),
+				SharedAccessStartTime:  to.Ptr(func() time.Time { t, _ := time.Parse(time.RFC3339Nano, today); return t }()),
+			}, nil)
+			if err != nil {
+				log.Fatal(err)
+				return
+			}
+			token := *sasToken.AccountSasToken
+			fmt.Printf("SAS Token %v\n", token)
+			return
 		} else {
-			log.Fatal(err)
+			fmt.Printf("Container Get status code %d error code: %v", respErr.StatusCode, respErr.ErrorCode)
+			log.Fatal(respErr)
 		}
 	} else {
 		id := *blobContainer.ID
